8933625e99f53e3eec2c40ff664d268151ff49ef
Telemachus <aashrafmohamed84@gmail.com>
2026-06-09 14:22:41 +0300

    remove task cmment

--- Lab-02/Lab-02-01.py
+++ Lab-02/Lab-02-01.py
@@ -1,8 +1,3 @@
-# - Given a list of numbers, create a function that returns a list where all similar adjacent
-# elements have been reduced to a single element, so [1,2,3.3] returns [1,2,3]
-# Note:
-# You may create a new list or modify the passed in list.
-
 def rm_adj(lst):
     if not lst:
         return []
